e3781e872f7992b3a68e8c54395201b7f932faed
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-01-28 11:03:04 -0300

    chore(build): update dependencies, enable Hilt and KSP, and migrate to Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,16 @@
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
 
-//    id("org.jetbrains.kotlin.kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.gabrielafonso.ipb.castelobranco"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36   // use 35/36 apenas se já estiver instalado no SDK Manager
 
     defaultConfig {
         applicationId = "com.gabrielafonso.ipb.castelobranco"
@@ -39,13 +37,16 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
@@ -53,13 +54,8 @@ android {
 
     packaging {
         resources {
-            // Remove o erro do INDEX.LIST
             excludes += "/META-INF/INDEX.LIST"
-
-            // Resolve o seu erro atual do Netty
             excludes += "/META-INF/io.netty.versions.properties"
-
-            // Evita erros comuns que costumam vir logo depois com essas bibliotecas
             excludes += "/META-INF/DEPENDENCIES"
             excludes += "/META-INF/LICENSE*"
             excludes += "/META-INF/NOTICE*"
@@ -72,36 +68,39 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    // Retrofit
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.9.6")
+
+    // Retrofit + OkHttp
     implementation(libs.retrofit)
-    // Conversor JSON
     implementation(libs.converter.gson)
-    implementation(libs.androidx.compose.runtime.livedata)
-    implementation(libs.androidx.compose.foundation.layout)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Kotlin Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    ksp("com.google.dagger:hilt-compiler:2.52")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:3.0.0")
-    implementation("androidx.navigation:navigation-compose:2.9.6")
-
-
-//    kapt(libs.androidx.room.compiler)
-
 }
