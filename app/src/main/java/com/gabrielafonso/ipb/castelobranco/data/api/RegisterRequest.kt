8e30deb913a5ad40aabe9c90e3a345d9a3c6264c
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-04 21:45:57 -0300

    refactor(api): update serialization annotations in RegisterRequest

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/data/api/RegisterRequest.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/data/api/RegisterRequest.kt
@@ -1,12 +1,17 @@
 package com.gabrielafonso.ipb.castelobranco.data.api
 
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RegisterRequest(
     val username: String,
+    @SerialName("first_name")
+    val firstName: String,
+    @SerialName("last_name")
+    val lastName: String,
     val password: String,
-    @SerializedName("password_confirm")
+    @SerialName("password_confirm")
     val passwordConfirm: String
 )
