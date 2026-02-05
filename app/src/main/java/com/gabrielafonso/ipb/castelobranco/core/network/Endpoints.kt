e2af8de9dc2011c04c6f8c8ab8f59d39910f2355
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-05 00:02:05 -0300

    feat(network): add AUTH_REFRESH_PATH endpoint

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/core/network/Endpoints.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/core/network/Endpoints.kt
@@ -13,4 +13,6 @@ object Endpoints {
 
     const val AUTH_LOGIN_PATH = "${ApiConstants.BASE_PATH}auth/login/"
     const val AUTH_REGISTER_PATH = "${ApiConstants.BASE_PATH}auth/register/"
+
+    const val AUTH_REFRESH_PATH = "${ApiConstants.BASE_PATH}auth/refresh/"
 }
