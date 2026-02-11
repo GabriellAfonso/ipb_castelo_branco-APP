06e10ef298e34c5a0f06860fcd8f53aa7c39291e
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-11 18:21:12 -0300

    feat(RefreshRequest): add serialization name for refresh field

--- app/src/main/java/com/gabrielafonso/ipb/castelobranco/data/api/RefreshRequest.kt
+++ app/src/main/java/com/gabrielafonso/ipb/castelobranco/data/api/RefreshRequest.kt
@@ -1,8 +1,9 @@
 package com.gabrielafonso.ipb.castelobranco.data.api
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class RefreshRequest(
-    val refresh: String
+    @SerialName("refresh") val refresh: String
 )
