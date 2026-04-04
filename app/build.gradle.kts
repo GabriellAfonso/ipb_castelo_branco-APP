72c4e3008584af11a04bef984be7ef3d1d38f24f
GabrielAfonso <gabriellafonso.dev@gmail.com>
2026-04-04 13:39:43 -0700

    chore(release): bump version to 0.7.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.ipb.castelobranco"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.7.4"
+        versionCode = 2
+        versionName = "0.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
