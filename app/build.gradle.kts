7a98cae2cadca34457dbe59258792923b4ac94b9
Gabriel Afonso <gabriellafonso.dev@gmail.com>
2026-05-06 18:51:45 -0300

    chore(release): bump version to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.ipb.castelobranco"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.8.2"
+        versionCode = 6
+        versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
